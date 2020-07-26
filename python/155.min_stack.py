f1edaa4562efb00abc18aa7def0d7ee04487c736
vritser <vritser@163.com>
2020-07-27 00:16:51 +0800

    155. min stack

---
+++ python/155.min_stack.py
@@ -0,0 +1,28 @@
+import sys
+
+# https://leetcode.com/problems/min-stack/
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = sys.maxsize
+
+
+    def push(self, x: int) -> None:
+        if x < self.min:
+            self.stack.append(self.min)
+            self.min = x
+        self.stack.append(x)
+
+
+    def pop(self) -> None:
+        if (self.stack.pop() == self.min):
+            self.min = self.stack.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        return self.min
