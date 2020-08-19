f71b85004fc795d9e5f21249dc86cbc1a0f73ae1
vritser <vritser@163.com>
2020-08-19 22:56:29 +0800

    206. reverse linked list

---
+++ python/206.reverse_linked_list.py
@@ -0,0 +1,21 @@
+
+from typedef.listnode import ListNode
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        dummy = None
+        while head:
+            next = head.next
+            head.next = dummy
+            dummy = head
+            head = next
+        return dummy
+
+    def reverseList2(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        prev = self.reverseList2(head.next)
+        head.next.next = head
+        head.next = None
+
+        return prev
---
+++ python/typedef/listnode.py
@@ -0,0 +1,5 @@
+
+class ListNode:
+    def __init__(self, val: int = 0, next: ListNode = None):
+        self.val = val
+        self.next = next
