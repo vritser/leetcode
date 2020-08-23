a85c092b9518efa1de774e9e97799571f94fc3b2
vritser <vritser@163.com>
2020-08-24 00:58:31 +0800

    283. move zeroes

---
+++ python/283.move_zeroes.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# https://leetcode.com/problems/move-zeroes
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k = 0
+        for i in range(0, len(nums)):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                k += 1
+
+        for j in rang(k, len(nums)):
+            nums[j] = 0
